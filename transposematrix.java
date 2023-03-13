944ffa0bc02b553a22d780c446b556c73b244057
AROU ISAAC <92853219+Arou355@users.noreply.github.com>
2023-03-13 14:43:06 +0300

    Update transposematrix.java

--- transposematrix.java
+++ transposematrix.java
@@ -1,12 +1,12 @@
 public class MatrixTransposeExample{  
     public static void main(String args[]){  
-    //creating a matrix  
+    //creating a matrix.
     int original[][]={{1,3,4},{2,4,3},{3,4,5}};    
         
-    //creating another matrix to store transpose of a matrix  
+    //creating another next matrix to store transpose of a matrix  
     int transpose[][]=new int[3][3];  //3 rows and 3 columns  
         
-    //Code to transpose a matrix  
+    //Code 😁to transpose a matrix  
     for(int i=0;i<3;i++){    
     for(int j=0;j<3;j++){    
     transpose[i][j]=original[j][i];  
